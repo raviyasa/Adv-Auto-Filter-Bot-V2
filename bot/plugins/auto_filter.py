c280725db2416157cb4a21bd327372819e8fe13c
raviyasa <88565232+raviyasa@users.noreply.github.com>
2021-11-03 11:01:22 +0845

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -153,7 +153,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"📂 Page 1/{len_result if len_result < max_pages else max_pages} 📂", callback_data="ignore")
         ])
         
         
@@ -203,7 +203,8 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>
+                Powered by @media_bot_updates",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
